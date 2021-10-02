937d5485dea5431344c00de12969cb0bb87aa34f
root <root@SidneyTan.localdomain>
2021-10-02 01:45:17 -0700

    First commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+const data = require('./data.json');
+
+//List of employee names.
+const employees = data.employees;
+//List of clocked in times.
+const clockedInTime = data.clockedInTime;
+//List of clocked out times.
+const clockedOutTime = data.cloeckOutTime;
+
+/*
+    Write your code down here. Feel free to change any of the variable names to your desire.
+*/
